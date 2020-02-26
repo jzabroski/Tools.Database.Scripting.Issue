41c63ee88ccccf49cb28e7ff736c034ac53fe1ef
John Zabroski <johnzabroski@gmail.com>
2020-02-26 18:33:17 -0500

    Repro

---
+++ Tools.Database.Scripting.Cli/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace Tools.Database.Scripting.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.ApplicationName = typeof(Program).Assembly.GetName().Name;
+            connectionStringBuilder.DataSource = "localhost";
+            connectionStringBuilder.InitialCatalog = "msdb";
+            connectionStringBuilder.IntegratedSecurity = true;
+
+            using (var connection = new SqlConnection(connectionStringBuilder.ConnectionString))
+            {
+                var serverConnection = new ServerConnection(connection);
+                var server = new Server(serverConnection);
+                var database = server.Databases["msdb"];
+                database.EnumObjectPermissions();
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
